ac25e86c07289d9cdbf874829437aab6e233b08b
Dmitry Shachnev <mitya57@gmail.com>
2014-05-24 17:30:10 +0400

    Simplify drawing code a bit

--- drawarea.cpp
+++ drawarea.cpp
@@ -5,14 +5,15 @@
 void DrawArea::paintEvent(QPaintEvent *event) {
     QWidget::paintEvent(event);
     QPainter painter(this);
-    double w = width(), h = height();
+    double wFactor = double(width()) / (stepsX - 1);
+    double hFactor = .5 * height();
     QPoint oldPoint, newPoint;
 
     for (unsigned stepX = 0; stepX < stepsX - 1; ++stepX) {
-        oldPoint.setX(w * stepX / stepsX);
-        oldPoint.setY(h * (.5 - data[stepT * stepsX + stepX] / 2));
-        newPoint.setX(w * (stepX + 1) / stepsX);
-        newPoint.setY(h * (.5 - data[stepT * stepsX + stepX + 1] / 2));
+        oldPoint.setX(wFactor * stepX);
+        oldPoint.setY(hFactor * (1. - data[stepT * stepsX + stepX]));
+        newPoint.setX(wFactor * (stepX + 1));
+        newPoint.setY(hFactor * (1. - data[stepT * stepsX + stepX + 1]));
         painter.drawLine(oldPoint, newPoint);
     }
 }
